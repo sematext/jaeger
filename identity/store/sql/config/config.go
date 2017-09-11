03f47cac17423bcc9e2ebc4f0266464b19ad1702
Nedim Šabić <rabbitstack7@gmail.com>
2017-09-11 17:53:03 +0200

    SPM-2124: adjust license text

--- identity/store/sql/config/config.go
+++ identity/store/sql/config/config.go
@@ -1,6 +1,5 @@
 //
 // Copyright (c) Sematext International
-// All Rights Reserved
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
