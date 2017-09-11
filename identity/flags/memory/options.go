73746c00d4fe2bbb6d30e4dc4e664ff3cda86351
Nedim Šabić <rabbitstack7@gmail.com>
2017-09-11 17:47:39 +0200

    SPM-2124: CLI options for in-memory token store

---
+++ identity/flags/memory/options.go
@@ -0,0 +1,82 @@
+//
+// Copyright (c) Sematext International
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package memory
+
+import (
+	"github.com/spf13/viper"
+	"flag"
+	"strings"
+	"github.com/uber/jaeger/identity/store/memory/config"
+)
+
+const (
+	suffixTokens    	= ".tokens"
+)
+
+// Options describes various configuration for the in-memory token store
+type Options struct {
+	primary *namespaceConfig
+}
+
+type namespaceConfig struct {
+	config.Configuration
+	namespace string
+}
+
+func NewOptions (namespace string) *Options {
+	return &Options {
+		primary: &namespaceConfig{
+			Configuration: config.Configuration{
+				Tokens: []string{},
+			},
+			namespace: namespace,
+		},
+	}
+}
+
+func (opt *Options) GetPrimary() *config.Configuration {
+	return &opt.primary.Configuration
+}
+
+// AddFlags adds flags for Options
+func (opt *Options) AddFlags(flagSet *flag.FlagSet) {
+	addFlags(flagSet, opt.primary)
+}
+
+func addFlags(flagSet *flag.FlagSet, nsConfig *namespaceConfig) {
+	flagSet.String(
+		nsConfig.namespace + suffixTokens,
+		"",
+		"The comma-separated list of authentication tokens")
+}
+
+// InitFromViper initializes Options with properties from viper
+func (opt *Options) InitFromViper(v *viper.Viper) {
+	initFromViper(opt.primary, v)
+}
+
+func initFromViper(cfg *namespaceConfig, v *viper.Viper) {
+	tokens := v.GetString(cfg.namespace + suffixTokens)
+	if tokens != "" {
+		cfg.Tokens = strings.Split(tokens, ",")
+	}
+}
