e39be7cfa8cfb987f9d0c8827b69c536f7c1a692
Nedim Šabić <rabbitstack7@gmail.com>
2017-09-11 13:01:02 +0200

    SPM-2124: DbTokenStore unit testing

--- identity/store/sql/sql_test.go
+++ identity/store/sql/sql_test.go
@@ -21,3 +21,103 @@
 // THE SOFTWARE.
 
 package sql
+
+import (
+	"testing"
+	"github.com/stretchr/testify/mock"
+	"go.uber.org/zap"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
+	"github.com/pkg/errors"
+)
+
+type clientMock struct {
+	mock.Mock
+}
+
+func (c clientMock) Ping() error {
+	args := c.Called()
+	return args.Error(0)
+}
+
+func (c clientMock) QueryForRow(query string, args ...interface{}) (interface{}, error) {
+	arguments := c.Called(query, args)
+	return arguments.String(0), arguments.Error(1)
+}
+
+func TestNewDbTokenStore(t *testing.T) {
+	c := new(clientMock)
+
+	c.On("Ping").Return(nil)
+
+	logger := zap.NewNop()
+	store, err := NewDbTokenStore(
+		c,
+		logger,
+		"SELECT token FROM system WHERE token = ?",
+		100,
+	)
+	require.NoError(t, err)
+	assert.NotNil(t, store)
+	assert.Equal(t, 0, store.cache.Size())
+}
+
+func TestNewDbTokenStorePingError(t *testing.T) {
+	c := new(clientMock)
+
+	c.On("Ping").Return(errors.New("Connection timeout"))
+
+	logger := zap.NewNop()
+	_, err := NewDbTokenStore(
+		c,
+		logger,
+		"SELECT token FROM system WHERE token = ?",
+		100,
+	)
+	require.Error(t, err)
+}
+
+func TestFindToken(t *testing.T) {
+	c := new(clientMock)
+
+	c.On("Ping").Return(nil)
+	c.On("QueryForRow",
+		"SELECT token FROM system WHERE token = ?",
+		[]interface{}{"c15a1793-71b7-46a5-88c5-bc76f9c772a0"},
+	).Return("c15a1793-71b7-46a5-88c5-bc76f9c772a0", nil)
+
+	logger := zap.NewNop()
+	store, _ := NewDbTokenStore(
+		c,
+		logger,
+		"SELECT token FROM system WHERE token = ?",
+		100,
+	)
+	assert.Nil(t, store.cache.Get("c15a1793-71b7-46a5-88c5-bc76f9c772a0"))
+	found, err := store.FindToken("c15a1793-71b7-46a5-88c5-bc76f9c772a0")
+	require.NoError(t, err)
+	assert.True(t, found)
+	assert.NotNil(t, store.cache.Get("c15a1793-71b7-46a5-88c5-bc76f9c772a0"))
+}
+
+func TestFindTokenQueryError(t *testing.T) {
+	c := new(clientMock)
+
+	c.On("Ping").Return(nil)
+	c.On("QueryForRow",
+		"SELECT token FROM system WHERE token = ?",
+		[]interface{}{"c15a1793-71b7-46a5-88c5-bc76f9c772a0"},
+	).Return("c15a1793-71b7-46a5-88c5-bc76f9c772a0", errors.New("sql: no rows in result set"))
+
+	logger := zap.NewNop()
+	store, _ := NewDbTokenStore(
+		c,
+		logger,
+		"SELECT token FROM system WHERE token = ?",
+		100,
+	)
+	assert.Nil(t, store.cache.Get("c15a1793-71b7-46a5-88c5-bc76f9c772a0"))
+	_, err := store.FindToken("c15a1793-71b7-46a5-88c5-bc76f9c772a0")
+	require.Error(t, err)
+	assert.Nil(t, store.cache.Get("c15a1793-71b7-46a5-88c5-bc76f9c772a0"))
+}
